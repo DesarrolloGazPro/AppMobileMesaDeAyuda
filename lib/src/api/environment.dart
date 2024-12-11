c63432264d9434208526e14e7c9741640216a04d
armandoferman <armando.ferman@gazpro.com.mx>
2024-12-11 14:55:34 -0700

    avances

--- lib/src/api/environment.dart
+++ lib/src/api/environment.dart
@@ -1,7 +1,7 @@
 class Environment {
 
   static const API_MESA_DE_AYUDA="http://10.0.2.2:5146/";
-  //static const API_MESA_DE_AYUDA="http://192.16.4.107:5146/";
+  //static const API_MESA_DE_AYUDA="https://grupogazpro.mx/ApiAppMesaDeAyuda/";
 
   
 
